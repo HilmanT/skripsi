8a429950bb186e6560b934938d5963a8717f09a0
Dean Alif Ahmad <deanalifahmad49@gmail.com>
2023-07-09 01:54:25 +0700

    make middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\EnsureAdminRole::class,
+        'dokter' => \App\Http\Middleware\EnsureDokterRole::class,
+        'pasien' => \App\Http\Middleware\EnsurePasienRole::class,
     ];
 }
